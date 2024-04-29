a951c69491ef254045c56c7200a346895ef70ce1
Muhannad Abdallah <Muhannadd159@gmail.com>
2024-04-29 22:44:07 +0300

    Edit Main Navigation UI

--- src/ui/MainNav.jsx
+++ src/ui/MainNav.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
-import { NavLink } from "react-router-dom";
-import styled from "styled-components";
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
 import {
   HiOutlineCalendarDays,
   HiOutlineCog6Tooth,
@@ -8,7 +8,7 @@ import {
   HiOutlineHomeModern,
   HiOutlineLink,
   HiOutlineUsers,
-} from "react-icons/hi2";
+} from 'react-icons/hi2';
 
 const NavList = styled.ul`
   display: flex;
@@ -60,33 +60,33 @@ function MainNav() {
     <nav>
       <NavList>
         <li>
-          <StyledNavLink to="/dashboard">
+          <StyledNavLink to='/dashboard'>
             <HiOutlineHome />
             <span>Home</span>
           </StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to="/bookings">
+          <StyledNavLink to='/bookings'>
             <HiOutlineCalendarDays />
             <span>Bookings</span>
           </StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to="/cabins">
-            <HiOutlineHomeModern />
-            <span>Cabins</span>
-          </StyledNavLink>
-        </li>
-        <li>
-          <StyledNavLink to="/users">
+          <StyledNavLink to='/cabins'>
             <HiOutlineUsers />
-            <span>Users</span>
+            <span>Patients</span>
           </StyledNavLink>
         </li>
         <li>
-          <StyledNavLink to="/settings">
-            <HiOutlineCog6Tooth />
-            <span>Settings</span>
+          <StyledNavLink to='/users'>
+            <HiOutlineUsers />
+            <span>Doctors</span>
+          </StyledNavLink>
+        </li>
+        <li>
+          <StyledNavLink to='/settings'>
+            <HiOutlineUsers />
+            <span>Nurses</span>
           </StyledNavLink>
         </li>
       </NavList>
