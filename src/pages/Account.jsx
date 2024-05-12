728e30194c7d2b13f803fc19521a042242310728
Muhannad Abdallah <Muhannadd159@gmail.com>
2024-05-13 00:30:05 +0300

    Update Account

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,20 +1,27 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
-import UpdateUserDataForm from "./../features/authentication/UpdateUserDataForm";
-import UpdatePasswordForm from "./../features/authentication/UpdatePasswordForm";
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+import UpdateUserDataForm from './../features/authentication/UpdateUserDataForm';
+import UpdatePasswordForm from './../features/authentication/UpdatePasswordForm';
+import { useUser } from '../features/authentication/useUser';
 
 function Account() {
+  const { user } = useUser();
+  let fullName = user.firstName + ' ' + user.lastName;
+  let role = user.role;
+  // show a div with the user's name, role, and email
   return (
     <>
-      <Heading as="h1">Update your account</Heading>
+      <Heading as='h1'>Welcome{fullName}</Heading>
+      <Heading as='h2'>Role: {role}</Heading>
+      <Heading as='h2'>Email: {user.email}</Heading>
 
       <Row>
-        <Heading as="h3">Update user data</Heading>
+        <Heading as='h3'>Update user data</Heading>
         <UpdateUserDataForm />
       </Row>
 
       <Row>
-        <Heading as="h3">Update password</Heading>
+        <Heading as='h3'>Update password</Heading>
         <UpdatePasswordForm />
       </Row>
     </>
